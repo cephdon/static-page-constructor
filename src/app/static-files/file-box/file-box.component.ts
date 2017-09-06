a3bbd1eb9fc5ba9c7cccf070e3dd1dceaa292ded
Platon Korzh <platonkorzh@Platons-MacBook-Pro.local>
2017-09-06 09:05:01 +0300

    static files WIP

---
+++ src/app/static-files/file-box/file-box.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-file-box',
+  templateUrl: './file-box.component.html',
+  styleUrls: ['./file-box.component.css']
+})
+export class FileBoxComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/static-files/static-files.module.ts
+++ src/app/static-files/static-files.module.ts
@@ -5,6 +5,8 @@ import { SharedModule } from './../shared/shared.module';
 
 import { StaticFilesRoutingModule } from './static-files-routing.module';
 import { StaticFilesComponent } from './static-files.component';
+import { FileControlComponent } from './file-control/file-control.component';
+import { FileBoxComponent } from './file-box/file-box.component';
 
 @NgModule({
 	imports: [
@@ -14,6 +16,8 @@ import { StaticFilesComponent } from './static-files.component';
 	],
 	declarations: [
 		StaticFilesComponent,
+		FileControlComponent,
+		FileBoxComponent,
 	]
 })
 export class StaticFilesModule { }
